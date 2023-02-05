b72838b3f76086a1be5b4a606a0e521bf1e27f1b
ML <mglee815@postech.ac.kr>
2023-02-05 21:49:22 +0900

    code sharing 0205

---
+++ code/clien_crawling/main.py
@@ -0,0 +1,36 @@
+import sys
+import json
+import openpyxl
+import configparser
+from tqdm import tqdm
+
+from excel import *
+from crawler import get_data
+
+keyword = ''
+result_path = ''
+
+if __name__ == "__main__":
+    keyword = input("키워드를 입력하세요 : ")
+    result_path = "../../data/cilen.xlsx"
+    print('============================')
+    print('키워드 :', keyword)
+    print('결과물 :', result_path)
+    print('============================')
+
+    try:
+        make_excel(result_path)
+        print('엑셀 파일 생성됨.')
+    except PermissionError:
+        print('엑셀 파일 생성 불가. 열려 있는 엑셀 파일을 종료해주세요.')
+        sys.exit(1)
+    print('============================')
+
+    start_idx = 1
+    with tqdm(total=100, ncols=80, desc='작업 진행') as pbar:
+        for item in get_data(keyword):
+            start_idx = append_excel(result_path, item, start_idx)
+            pbar.update(1)
+    print('============================')
+    print('작업 완료됨. 프로그램을 종료해주세요.')
+    input()
